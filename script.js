5059a5c0da484ac595d0b963ad2501ee4d3b02f3
ecocaval <ecocaval@hotmail.com>
2022-10-12 20:41:50 -0300

    working jquery

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 // Main dishes section
-const main_dishes = document.querySelectorAll('.main_dish');
+const dishes = document.querySelectorAll('.dish');
 
 // function starts when users click any main dish
-$(main_dishes).click(
+$(dishes).click(
     function() {      
         // look_for_check_mark_in(this.className[0]) 
-        let class_name = this.className;
+        let class_name = this.className.slice(5);
         let found_check_marks = false;
+
+        console.log(class_name);
 
         $(`.${class_name}`).each(
             function(index, element) {
@@ -30,48 +32,4 @@ $(main_dishes).click(
         }
     }
 );
-
-// Main dishes section
-const drink_dishes = document.querySelectorAll('.drink_dish');
-
-// function starts when users click any main dish
-$(drink_dishes).click(
-    function() {      
-        // check's if selector has check mark
-        if(!($(this).find(".dish_check").attr("class").includes('dish_check_appear'))) {     
-            // adds check mark to selector
-            $(this).find(".dish_check").addClass('dish_check_appear');
-            $(this).find(".dish_check").removeClass('dish_check_disappear');
-            $(this).addClass('dish_box_check');     
-        } else {
-            // removes check mark from selector
-            $(this).find(".dish_check").removeClass('dish_check_appear');
-            $(this).find(".dish_check").addClass('dish_check_disappear');
-            $(this).removeClass('dish_box_check');
-        }
-    }
-);
-
-
-
-// Main dishes section
-const dessert_dishes = document.querySelectorAll('.dessert_dish');
-
-// function starts when users click any main dish
-$(dessert_dishes).click(
-    function() {      
-        // check's if selector has check mark
-        if(!($(this).find(".dish_check").attr("class").includes('dish_check_appear'))) {     
-            // adds check mark to selector
-            $(this).find(".dish_check").addClass('dish_check_appear');
-            $(this).find(".dish_check").removeClass('dish_check_disappear');
-            $(this).addClass('dish_box_check');     
-        } else {
-            // removes check mark from selector
-            $(this).find(".dish_check").removeClass('dish_check_appear');
-            $(this).find(".dish_check").addClass('dish_check_disappear');
-            $(this).removeClass('dish_box_check');
-        }
-    }
-);
-
+z
