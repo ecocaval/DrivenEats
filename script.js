90cea2bb81d14f21f2ac6a20dcb33f1396aa5a58
ecocaval <ecocaval@hotmail.com>
2022-10-12 19:06:44 -0300

    adding clean jquery to main dishes

--- script.js
+++ script.js
@@ -1,35 +1,14 @@
-
-
-$('.main_dish').click(
-    function() {
-        
-        // checks if current dish has check mark
-
-        if ( !($('.main_dish .dish_check').attr("class").includes('dish_check_appear'))) {
-
-            $('.main_dish').each(
-                function(index, element){
-                    $(this).find(".dish_check").addClass('dish_check_appear');
-                    $(this).find(".dish_check").removeClass('dish_check_disappear')
-                    $(this).addClass('dish_box_check')
-                }
-            )
-
+// function starts when users click any main dish
+$(document.querySelectorAll('.main_dish')).click(
+    function() {        
+        if(!($(this).find(".dish_check").attr("class").includes('dish_check_appear'))) {
+            $(this).find(".dish_check").addClass('dish_check_appear');
+            $(this).find(".dish_check").removeClass('dish_check_disappear');
+            $(this).addClass('dish_box_check');
         } else {
-            // removes check mark and green borders from current dish
-           
-            $('.main_dish').each(
-                function(index, element){
-                    $(this).find(".dish_check").removeClass('dish_check_appear');
-                    $(this).find(".dish_check").addClass('dish_check_disappear')
-                    $(this).removeClass('dish_box_check')
-                }
-            )
-
+            $(this).find(".dish_check").removeClass('dish_check_appear');
+            $(this).find(".dish_check").addClass('dish_check_disappear');
+            $(this).removeClass('dish_box_check');
         }
     }
 );
-
-
-
-
