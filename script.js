20e20a04f38fb8e4eef4545be69f2e6a0659c2de
ecocaval <ecocaval@hotmail.com>
2022-10-12 18:43:29 -0300

    cleaning jquery code

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
-//$('.dish').fadeOut("2000");
-    
-$('.dish').click(
-    function() {
-        // checks if current dish has check mark
-        if ( !($('.dish_check').attr("class").includes('dish_check_appear'))) {
-            // adds check mark to current dish
-            $('.dish_check').addClass('dish_check_appear');
-            $('.dish_check').removeClass('dish_check_disappear');
-            $('.dish').addClass('main_dish');
-        } else {
-            // removes check mark from current dish
-            $('.dish_check').removeClass('dish_check_appear');
-            $('.dish_check').addClass('dish_check_disappear');
-            $('.dish').removeClass('main_dish');
-        }
 
+
+$('.main_dish').click(
+    function() {
+        
+        // checks if current dish has check mark
+
+        if ( !($('.main_dish .dish_check').attr("class").includes('dish_check_appear'))) {
+
+            $('.main_dish').each(
+                function(index, element){
+                    $(this).find(".dish_check").addClass('dish_check_appear');
+                    $(this).find(".dish_check").removeClass('dish_check_disappear')
+                    $(this).addClass('dish_box_check')
+                }
+            )
+
+        } else {
+            // removes check mark and green borders from current dish
+           
+            $('.main_dish').each(
+                function(index, element){
+                    $(this).find(".dish_check").removeClass('dish_check_appear');
+                    $(this).find(".dish_check").addClass('dish_check_disappear')
+                    $(this).removeClass('dish_box_check')
+                }
+            )
+
+        }
     }
 );
+
+
 
 
