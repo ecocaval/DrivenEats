9ae22f21a4506d6e1c90d12811b3a68ef277bc33
ecocaval <ecocaval@hotmail.com>
2022-10-15 01:06:52 -0300

    cleaning code and improving variable names

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const dishes = Array.from(document.querySelectorAll('.dish\n\n\n'));
 
 // this function is called when the user clicks a meal
 function meal_clicked(meal) {
+    // gets the second class of the meal getting clicked, in this case it can be main, drink or dessert
     const dish_option = meal.classList[1];
     const meal_is_clicked = meal.classList.contains('dish_box_check');
 
@@ -14,10 +15,12 @@ function meal_clicked(meal) {
         look_for_selected_meals(option_dishes);
     }
     
+    // adds or removes green check box and green check image
     add_remove_check(meal);
 
     // checks if order is over, in order to control final order button
     if(check_if_order_is_over()) {
+        // changes button classes so it turns green, also changes it's text  
         release_order_button();
     } else {
         hold_order_button();
@@ -26,10 +29,12 @@ function meal_clicked(meal) {
 
 // looks for meals of the same type that are selected, then unselect them
 function look_for_selected_meals(dishes_to_look) {
-    for(let i = 0; i < dishes_to_look.length; i++) {
-        // checks if dish is selected looking for the selected class
+    for(let i in dishes_to_look) {
+        // checks if dish is selected looking for the selected box class
         if(dishes_to_look[i].classList.contains('dish_box_check')) {
+            // adds or removes green check box and green check image
             add_remove_check(dishes_to_look[i]);
+            // breaks the function since only one meal can be selected
             break;
         }
     }
@@ -37,22 +42,20 @@ function look_for_selected_meals(dishes_to_look) {
 
 // adds or removes green check box and green check image
 function add_remove_check(meal) {
-    meal.classList.toggle('dish_box_check');
-    meal.childNodes[9].classList.toggle('display_none');
+    meal.classList.toggle('dish_box_check'); // green box in meal div
+    meal.childNodes[9].classList.toggle('display_none'); // green check
 }
 
 // checks if there are 3 meals selected
 function check_if_order_is_over() {
     let meals_selected = 0;
-    for(let i = 0; i < dishes.length; i++) {
+    for(let i in dishes) {
         if(dishes[i].classList.contains('dish_box_check')) {
             meals_selected++;
         }
     }
-    if(meals_selected === 3) {
-        return true;
-    }
-    return false;
+    // the order is only considered finished if 3 meals are selected
+    return (meals_selected === 3);
 }
 
 // releases final orden button
@@ -80,9 +83,9 @@ function release_confirm_section(){
     const meals_prices_selected = check_for_meals_selected()[1];
 
     // arrays containing all html tags elements
-    const meals_names = Array.from(document.querySelectorAll('.last_meal_name'));
-    const meals_prices = Array.from(document.querySelectorAll('.last_meal_price')); 
-    const meal_total_price_select = document.querySelector('.last_meal_total_price');
+    const meals_names = Array.from(document.querySelectorAll('.final_order_meal_name'));
+    const meals_prices = Array.from(document.querySelectorAll('.final_order_meal_price')); 
+    const meal_total_price_select = document.querySelector('.final_order_meal_total_price');
     const order_button = document.querySelector('.order_button');
     const confirm_section = document.querySelector('.confirm_section');
     const transparent_background = document.querySelector('.transparent_background');
@@ -91,7 +94,7 @@ function release_confirm_section(){
     change_confirm_section_param(meals_names_selected, meals_names, meals_prices_selected, meals_prices);
 
      // changes the total price value in the HTML
-    meal_total_price_select.innerHTML = calculate_order_total_price(meals_prices_selected, );
+    meal_total_price_select.innerHTML = calculate_order_total_price(meals_prices_selected);
 
     if(order_button.classList.contains('order_button_ready')){
         confirm_section.classList.remove('display_none');
@@ -101,7 +104,7 @@ function release_confirm_section(){
 
 function change_confirm_section_param(meals_names_selected, meals_names, meals_prices_selected, meals_prices){
     change_meals_names(meals_names_selected, meals_names);
-    change_meals_prices(meals_prices_selected, meals_prices)
+    change_meals_prices(meals_prices_selected, meals_prices);
 }
 
 function change_meals_names(meals_names_selected, meals_names){
@@ -136,8 +139,10 @@ function cancel_order(){
 }
 
 function ask_name_and_adress(){
-    // checks the names of the meals that are selected [0] names, [1] prices
+    // checks the names and prices of the meals that are selected 
+    // [0] names, [1] prices
     const meals_names_selected = check_for_meals_selected()[0];
+    const meals_prices_selected = check_for_meals_selected()[1];
     
     const name = prompt("Por favor digite o seu nome: ");    
     const addres = prompt("Por favor digite o seu endereço: ");
@@ -146,7 +151,7 @@ function ask_name_and_adress(){
     - Prato: ${meals_names_selected[0]}
     - Bebida: ${meals_names_selected[1]}
     - Sobremesa: ${meals_names_selected[2]}
-    Total: R$ 27.70
+    Total: R$ ${calculate_order_total_price(meals_prices_selected)}
     
     Nome: ${name}
     Endereço: ${addres}`;
@@ -165,7 +170,6 @@ function check_for_meals_selected() {
         if(dishes[i].classList.contains('dish_box_check')) {
             meals_selected_names.push(dishes[i].childNodes[3].innerHTML);
             meals_selected_prices.push((dishes[i].childNodes[7].innerHTML.slice(3)).replace(",","."));
-            
         }
     }    
     // creates a matrix with all meals selected info
